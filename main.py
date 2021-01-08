1235d72b6b527824519a2a37f3c53bd6cbe45693
Marc Castells <marc@castells.co>
2021-01-08 15:27:48 -0500

    removed f-string debugging specifiers for pre-python3.8 compatibility

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ storms = hurdat.listFromFile('HURDATHurricaneTracks.txt')
 n = 3
 print(f'{len(storms)} storms recorded. Last {n} storms:')
 for storm in storms[-n:]:
-    print(f'\n{storm.id = }')
-    print(f'{storm.name = }')
-    print(f'{storm.record_count = }')
+    print(f'\nstorm.id = {storm.id}')
+    print(f'storm.name = {storm.name}')
+    print(f'storm.record_count = {storm.record_count}')
     for record in storm.records:
-        print(f'\n{record.datetime.strftime("%c") = }')
-        print(f'{record.year = }, {record.month = }, {record.day = }, {record.hour = }')
-        print(f'{record.lat = }, {record.lon = }')
-        print(f'{record.minpressure = }, {record.maxwspeed = }')
+        print(f'\nrecord.datetime.strftime("%c") = {record.datetime.strftime("%c")}')
+        print(f'record.year = {record.year}, record.month = {record.month}, record.day = {record.day}, record.hour = {record.hour}')
+        print(f'record.lat = {record.lat}, record.lon = {record.lon}')
+        print(f'record.minpressure = {record.minpressure}, record.maxwspeed = {record.maxwspeed}')
         #etc...
